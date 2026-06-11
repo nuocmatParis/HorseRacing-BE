6688a84bf6f888a86edd2847d57bd404b707563a
phun <tranphuhung1510@gmail.com>
2026-06-11 21:12:00 +0700

    exception handle

---
+++ src/main/java/com/swp391/horseracing/exception/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.swp391.horseracing.exception;
+
+import com.swp391.horseracing.dto.common.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.swing.text.html.parser.Entity;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = AppException.class)
+    public ResponseEntity<ApiResponse<Void>> handleAppException(AppException exception){
+        ErrorCode errorCode = exception.getErrorCode();
+
+        ApiResponse<Void> apiResponse = ApiResponse.<Void>builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception exception){
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+
+        ApiResponse<Void> apiResponse = ApiResponse.<Void>builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(apiResponse);
+    }
+
+}
