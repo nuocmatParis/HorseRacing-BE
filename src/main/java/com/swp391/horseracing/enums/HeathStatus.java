998e6a1ad05dc78ed9ee17210cf0e46112ae0a7b
phun <tranphuhung1510@gmail.com>
2026-06-11 21:35:35 +0700

    fix enum class

--- src/main/java/com/swp391/horseracing/enums/HeathStatus.java
+++ src/main/java/com/swp391/horseracing/enums/HeathStatus.java
@@ -1,7 +1,7 @@
 package com.swp391.horseracing.enums;
 
 public enum HeathStatus {
-    Heathy,
-    Injured,
-    Retired
+    HEALTHY,
+    INJURED,
+    RETIRED
 }
--- src/main/java/com/swp391/horseracing/enums/RefereeStatus.java
+++ src/main/java/com/swp391/horseracing/enums/RefereeStatus.java
@@ -1,7 +1,7 @@
 package com.swp391.horseracing.enums;
 
 public enum RefereeStatus {
-    Available,
-    Assigned,
-    Suspended
+    AVAILABLE,
+    ASSIGNED,
+    SUSPENDED
 }
