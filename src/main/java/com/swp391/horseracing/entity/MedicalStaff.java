bac8d63ce1266658bfaa0f6330015c4b04cb55f4
Hoàng Hải Nguyễn Vũ <hoanghai>
2026-06-11 16:58:48 +0700

    feat: configure MySQL database connection and snake_case

--- src/main/java/com/swp391/horseracing/entity/MedicalStaff.java
+++ src/main/java/com/swp391/horseracing/entity/MedicalStaff.java
@@ -3,16 +3,15 @@ package com.swp391.horseracing.entity;
 import com.swp391.horseracing.enums.RefereeStatus;
 import jakarta.persistence.*;
 
-@Table(name = "MedicalStaff")
+@Table(name = "medical_staff")
 @Entity
-@PrimaryKeyJoinColumn(name = "UserId")
+@PrimaryKeyJoinColumn(name = "user_id")
 public class MedicalStaff extends User {
-    @Column(name = "LicenseNumber", nullable = false, length = 50)
+    @Column(name = "license_number", nullable = false, length = 50)
     private String licenseNumber;
-    @Column(name = "YearOfService", nullable = false)
+    @Column(name = "year_of_service", nullable = false)
     private int yearOfService;
     @Enumerated(EnumType.STRING)
-    @Column(name = "Status")
-    private RefereeStatus Status;
-
+    @Column(name = "status")
+    private RefereeStatus status;
 }
--- src/main/java/com/swp391/horseracing/entity/Referee.java
+++ src/main/java/com/swp391/horseracing/entity/Referee.java
@@ -3,15 +3,14 @@ package com.swp391.horseracing.entity;
 import com.swp391.horseracing.enums.RefereeStatus;
 import jakarta.persistence.*;
 
-@Table(name = "Referee")
+@Table(name = "referee")
 @Entity
-@PrimaryKeyJoinColumn(name = "UserId")
+@PrimaryKeyJoinColumn(name = "user_id")
 public class Referee extends User {
-    @Column(name = "Certification", nullable = false,length = 50)
+    @Column(name = "certification", nullable = false, length = 50)
     private String certification;
-    @Column(name = "YearOfService", nullable = false)
+    @Column(name = "year_of_service", nullable = false)
     private int yearOfService;
-    @Column(name = "Status")
+    @Column(name = "status")
     private RefereeStatus status;
-
 }
