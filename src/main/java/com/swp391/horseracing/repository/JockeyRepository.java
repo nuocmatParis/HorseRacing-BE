d72a457a2455ed37af7e7315951e95733b7f29b4
Hoàng Hải Nguyễn Vũ <hoanghai>
2026-06-15 02:01:51 +0700

    feat: implement Horse, Jockey CRUD APIs and DataSeeder

---
+++ src/main/java/com/swp391/horseracing/repository/JockeyRepository.java
@@ -0,0 +1,16 @@
+package com.swp391.horseracing.repository;
+
+import com.swp391.horseracing.entity.Jockey;
+import com.swp391.horseracing.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface JockeyRepository extends JpaRepository<Jockey, UUID> {
+    Optional<Jockey> findByUser_UserId(UUID userId);
+    boolean existsByUser_UserId(UUID userId);
+
+}
