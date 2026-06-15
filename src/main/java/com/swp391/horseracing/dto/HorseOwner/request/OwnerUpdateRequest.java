0e04a2c5e69005ee7a9c8e91de50612dc4082e5c
hoanghai100305-code <hoanghai100305@gmail.com>
2026-06-15 20:00:56 +0700

    feat: implement Jockey and HorseOwner profile APIs

---
+++ src/main/java/com/swp391/horseracing/dto/HorseOwner/request/OwnerUpdateRequest.java
@@ -0,0 +1,23 @@
+package com.swp391.horseracing.dto.HorseOwner.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class OwnerUpdateRequest {
+    @Size(max = 100)
+    String farmName;
+
+    String address;
+
+    @Size(max = 50)
+    String licenseNumber;
+}
