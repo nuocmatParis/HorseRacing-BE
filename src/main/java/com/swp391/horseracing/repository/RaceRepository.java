282e5f22bf4723e8b9454d3f08e2f66f0ff20573
Hoàng Hải Nguyễn Vũ <hoanghai>
2026-06-25 00:59:23 +0700

    feat: add tournament schedule APIs and phase timeline validation

--- src/main/java/com/swp391/horseracing/repository/RaceRepository.java
+++ src/main/java/com/swp391/horseracing/repository/RaceRepository.java
@@ -1,6 +1,5 @@
 package com.swp391.horseracing.repository;
 
-import com.swp391.horseracing.dto.tournament.response.RaceResponse;
 import com.swp391.horseracing.entity.Race;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
