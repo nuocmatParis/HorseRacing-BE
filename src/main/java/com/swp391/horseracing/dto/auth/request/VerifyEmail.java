5bd90011179361f333c81f3ae43343f183748cbf
phun <tranphuhung1510@gmail.com>
2026-06-26 19:23:05 +0700

    verification email otp code

---
+++ src/main/java/com/swp391/horseracing/dto/auth/request/VerifyEmail.java
@@ -0,0 +1,21 @@
+package com.swp391.horseracing.dto.auth.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class VerifyEmailRequest {
+
+    @Email(message = "Email is invalid")
+    @NotBlank(message = "Email is required")
+    String email;
+
+    @NotBlank(message = "OTP code is required")
+    String otpCode;
+}
