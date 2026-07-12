e6e1432d8f53dfb610a9f81ba154974089583459
hoanghai100305-code <hoanghai100305@gmail.com>
2026-07-12 22:26:24 +0700

    resolve confict

--- src/main/java/com/swp391/horseracing/service/impl/RaceServiceImpl.java
+++ src/main/java/com/swp391/horseracing/service/impl/RaceServiceImpl.java
@@ -67,6 +67,17 @@ public class RaceServiceImpl implements RaceService {
         if (raceRepository.existsByRound_RoundIdAndName(roundId, request.getName())) {
             throw new AppException(ErrorCode.RACE_NAME_ALREADY_EXISTS);
         }
+        if(round.getMaxRaces() <= round.getRaces().size()){
+            throw new AppException(ErrorCode.MAX_RACES_REACHED);
+        }
+
+        List<Race> existingRaces = raceRepository.findByRound_RoundIdOrderByStartTimeDesc(roundId);
+        if (!existingRaces.isEmpty()) {
+            Race lastRace = existingRaces.get(0);
+            if (request.getStartTime().isBefore(lastRace.getEndTime())) {
+                throw new AppException(ErrorCode.RACE_DATES_OUT_OF_ROUND);
+            }
+        }
 
         if (raceRepository.existsByRound_RoundIdAndSequenceOrder(roundId, request.getSequenceOrder())) {
             throw new AppException(ErrorCode.DUPLICATE_RACE_SEQUENCE);
